df3b4e64351ddbed5ab2e5839d3de9aebc541396
akmatoff <azim.akmatov1@gmail.com>
2021-05-31 17:07:30 +0600

    Home page made

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,20 @@ import '../App.css'
 
 function Home() {
     return(
-        <div className="main-container" id="home">
+        <div className="main-container flex-column" id="home">
+            
+            <div className="home-text-container">
+                <h1 className="greeting">AZIM AKMATOV</h1>
+                <p className="greeting-about">
+                    FULL STACK DEVELOPER
+                </p>
+
+                <div className="social-links-container flex-row">
+                    <a href="https://github.com/akmatoff" target="_blank" rel="noreferrer"><i className="fab fa-github social-icon"></i></a>
+                    <a href="https://t.me/akmatoffexe" target="_blank" rel="noreferrer"><i className="fab fa-telegram-plane social-icon"></i></a>
+                    <a href="mailto: akmatoff.exe@gmail.com"><i className="fas fa-envelope social-icon"></i></a>
+                </div>
+            </div>
 
         </div>
     )
