c31c05fb2b10d1fef828a64647f7ce3282fbbb1c
akmatoff <azim.akmatov1@gmail.com>
2021-06-30 16:33:45 +0600

    About section update, snap update

--- src/components/About.js
+++ src/components/About.js
@@ -19,9 +19,15 @@ function About() {
     >
       <h1 className="header-title">About</h1>
       <div className="text-container">
-        I'm a beginning JavaScript developer with some experience in React,
-        Express and ThreeJS. I build REST APIs using Express and Single Page
-        Applications with React. Also build mobile apps with Flutter.
+        I'm a beginning JavaScript developer with some experience in React and
+        Express. I'm here to build REST APIs using Express and Single Page
+        Applications with React. I also design mobile apps with Flutter.
+      </div>
+      <div className="text-container">
+        After spending years on not knowing exactly in what direction I want to
+        move, looking for right technologies that I'd like to use I've come up
+        with web development using JavaScript and Mobile Development using
+        Flutter.
       </div>
     </div>
   );
