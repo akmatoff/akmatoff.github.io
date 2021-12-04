eab969f31ed757663f0b7628421e1e2fa2be1bdf
akmatoff <akmatoff.exe@gmail.com>
2021-12-04 15:33:50 +0600

    UPDATE: colors

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -24,24 +24,26 @@ function Projects() {
       <div className="projects-container">
         {projects.sort((p, p2) => p2.project_id - p.project_id).map((project) => (
           <div key={project.project_id} className="project-card">
-            <div className="title">{project.project_title}</div>
-            <div className="type">{project.project_type}</div>
-            <div className="technology">{project.technology}</div>
-            <div className="description">{project.description}</div>
-            <div className="links">Links:</div>
-            <div className="project-links flex-row">
-              {project.repositories.map((repo, index) => (
-                <a key={index} href={repo} rel="noreferrer" target="_blank">
-                  <i className="fab fa-github project-icon"></i>
-                </a>
-              ))}
-              {project.app ? (
-                <a href={project.app} rel="noreferrer" target="_blank">
-                  <i className="fas fa-globe project-icon"></i>
-                </a>
-              ) : null}
+      
+              <div className="title">{project.project_title}</div>
+
+              <div className="type">{project.project_type}</div>
+              <div className="technology">{project.technology}</div>
+              <div className="description">{project.description}</div>
+              <div className="links">Links:</div>
+              <div className="project-links flex-row">
+                {project.repositories.map((repo, index) => (
+                  <a key={index} href={repo} rel="noreferrer" target="_blank">
+                    <i className="fab fa-github project-icon"></i>
+                  </a>
+                ))}
+                {project.app ? (
+                  <a href={project.app} rel="noreferrer" target="_blank">
+                    <i className="fas fa-globe project-icon"></i>
+                  </a>
+                ) : null}
+              </div>
             </div>
-          </div>
         ))}
       </div>
     </div>
