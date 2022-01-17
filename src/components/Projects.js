e830bc24fbd5adf9b2ce0ca3c749dfaaa67b2d79
akmatoff <akmatoff.exe@gmail.com>
2022-01-18 00:23:10 +0600

    UPDATE: Screenshots for projects, new project cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,36 +15,53 @@ function Projects() {
   return (
     <div
       ref={projectsContainer}
-      className="main-container flex-column centered"
+      className="main-container flex-column"
       id="projects"
     >
       <h1 className="header-title" id="projectsTitle">
         Projects
       </h1>
-      <div className="projects-container">
-        {projects.sort((p, p2) => p2.project_id - p.project_id).map((project) => (
-          <div key={project.project_id} className="project-card">
-      
-              <div className="title">{project.project_title}</div>
+      <div className="projects-container flex-column">
+        {projects
+          .sort((p, p2) => p2.project_id - p.project_id)
+          .map((project) => (
+            <div
+              key={project.project_id}
+              className="project-card flex-row centered"
+            >
+              <div className="project-card__text flex-column">
+                <div className="title">{project.project_title}</div>
 
-              <div className="type">{project.project_type}</div>
-              <div className="technology">{project.technology}</div>
-              <div className="description">{project.description}</div>
-              <div className="links">Links:</div>
-              <div className="project-links flex-row">
-                {project.repositories.map((repo, index) => (
-                  <a key={index} href={repo} rel="noreferrer" target="_blank">
-                    <i className="fab fa-github project-icon"></i>
-                  </a>
-                ))}
-                {project.app ? (
-                  <a href={project.app} rel="noreferrer" target="_blank">
-                    <i className="fas fa-globe project-icon"></i>
-                  </a>
-                ) : null}
+                <div className="type">{project.project_type}</div>
+                <div className="technology">{project.technology}</div>
+                <div className="description">{project.description}</div>
+                <div className="links">Links:</div>
+                <div className="project-links flex-row">
+                  {project.repositories.map((repo, index) => (
+                    <a key={index} href={repo} rel="noreferrer" target="_blank">
+                      <i className="fab fa-github project-icon"></i>
+                    </a>
+                  ))}
+                  {project.app ? (
+                    <a href={project.app} rel="noreferrer" target="_blank">
+                      <i className="fas fa-globe project-icon"></i>
+                    </a>
+                  ) : null}
+                </div>
+              </div>
+              <div
+                className="picture flex-row centered"
+                style={{
+                  background: `url(${project.picture})`,
+                  backgroundSize: "110%",
+                  backgroundPosition: "center",
+                  backgroundRepeat: "no-repeat",
+                }}
+              >
+                {project.picture ? "" : project.project_title}
               </div>
             </div>
-        ))}
+          ))}
       </div>
     </div>
   );
