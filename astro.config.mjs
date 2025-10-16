4d1cbf2c68e70fbfdbfba0ee33ac5b04aaae542f
akmatoff <azim.akmatov1@gmail.com>
2025-10-16 23:40:06 +0600

    add github pages deploy, CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from 'astro-icon'
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
+  site: 'https://akmatoff.cv',
+  base: "/website",
 });
