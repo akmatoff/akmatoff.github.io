f04988b3025b7f28874503fd7b3b0bf9964978c1
akmatoff <azim.akmatov1@gmail.com>
2025-10-17 14:29:55 +0600

    update CNAME and astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import icon from 'astro-icon'
+import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  site: 'https://akmatoff.cv',
-  base: "/website",
+  site: "https://akmatoff.cv",
+  base: "/",
 });
